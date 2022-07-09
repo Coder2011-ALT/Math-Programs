66dca91c93af9166d08cbaafeb28e1f7ae5afde6
Gyanesh Satyawali <gyaneshsatyawali30@gmail.com>
2022-07-09 09:12:19 +0530

    Added comments, made the code better.

--- divisibility.py
+++ divisibility.py
@@ -1,17 +1,21 @@
-import time
+# This calculator checks that from which number the number is divisible by and checks the number whether it is prime or composite.
+# Made by Coder2011-ALT In 2022
+# Defining variables
+divisibilityList = []
 num = int(input("Write the number of which you want to check divisibility of-\n"))
-divisibilityNum = int(input("Write the number that you want the number is divisible to numbers till-\n"))
 prime = 0
-for i in range(1, divisibilityNum):
+# Running the forloop to check the factors of the number.
+for i in range(1, num+1):
     if num % i == 0:
         print(f"{i} is divisible by {num}")
-if divisibilityNum < num:
-    print(f"{num} is divisible by {num}")
-for j in range(2, num):
-    prime += 1
+        divisibilityList.append(i)
+        prime += 1
+# Checking if the number is prime or composite
 if prime > 2:
     print(f"{num} is a composite number.")
 elif prime == 2:
     print(f"{num} is a prime number.")
 elif prime < 2:
     print(f"{num} is prime nor composite number")
+# Printing the factors of the number.
+print(f"The numbers - \n{divisibilityList} are divisible by {num}")
