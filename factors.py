78e387cfa2dd1bc3bdbc4689b98bdc76ed2f0c41
Gyanesh Satyawali <gyaneshsatyawali30@gmail.com>
2022-12-07 21:49:12 +0530

    A new program added

--- factors.py
+++ factors.py
@@ -1,21 +1,28 @@
-# This calculator checks that from which number the number is divisible by and checks the number whether it is prime or composite.
+# This calculator finds the factors of the number.
 # Made by Coder2011-ALT In 2022
+
 # Defining variables
 divisibilityList = []
 num = int(input("Write the number of which you want to check divisibility of-\n"))
-prime = 0
+factors = 0
+
 # Running the forloop to check the factors of the number.
 for i in range(1, num+1):
     if num % i == 0:
-        print(f"{i} is divisible by {num}")
+        print(f"{i} is a factor of {num}")
         divisibilityList.append(i)
-        prime += 1
+        factors += 1
+
 # Checking if the number is prime or composite
-if prime > 2:
+if factors > 2:
     print(f"{num} is a composite number.")
-elif prime == 2:
+elif factors == 2:
     print(f"{num} is a prime number.")
-elif prime < 2:
+elif factors < 2:
     print(f"{num} is prime nor composite number")
+
 # Printing the factors of the number.
-print(f"The numbers - \n{divisibilityList} are divisible by {num}")
+for j in divisibilityList:
+    print(f"{j}",end=",")
+print(f" are the factors of {num}")
+print("Thank you!")
---
+++ hcf.py
@@ -0,0 +1,35 @@
+# This calculator finds the HCF of any given numbers.
+# Made by Coder2011-ALT In Dec 2022
+# Defining variables
+divisibilityList1 = []
+divisibilityList2 = []
+commonF = []
+
+# Program heading
+print("This is a HCF calculator.")
+
+# Taking the input of the user
+num1 = int(input("Write the first number-\n"))
+num2 = int(input("Write the second number-\n"))
+
+# Running the forloop to check the factors of the number.
+for i in range(1, num1+1):
+    if num1 % i == 0:
+        divisibilityList1.append(i)
+for j in range(1, num2+1):
+    if num2 % j == 0:
+        divisibilityList2.append(j)
+
+# Running the forloop to check and append the common factors of the numbers
+for k in divisibilityList1:
+    for g in divisibilityList2:
+        if g == k:
+            commonF.append(g)
+hcf = max(commonF)
+
+# Checking and printing if the number are coprimes
+if hcf != 1:
+    print(f"The HCF of {num1} and {num2} is- \n{hcf}")
+else:
+    print(f"{num1} and {num2} are coprimes.")
+print("Thank you!")
